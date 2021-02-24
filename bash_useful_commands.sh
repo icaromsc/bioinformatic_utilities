be8163f88c76496cc7dc8497a18548739aac95d7
Ícaro M. S. Castro <icaromscastro@gmail.com>
2021-02-23 23:47:05 -0300

    Update bash_useful_commands.sh

--- bash_useful_commands.sh
+++ bash_useful_commands.sh
@@ -1,2 +1,11 @@
-#Replacing spaces in the filenames with underscores
+#Replace spaces in filenames with underscores
 for i in *' '*; do   mv "$i" `echo $i | sed -e 's/ /_/g'`; done
+
+# Run command in background
+Rscript 03_normalization_and_clustering.R & 
+
+# Run command in background and redirect output to a file nohup.out
+nohup Rscript 03_normalization_and_clustering.R & 
+
+# Run command in background and redirect output to a file called 03_cite_seq.log
+nohup Rscript 03_normalization_and_clustering.R &> 03_cite_seq.log & 
